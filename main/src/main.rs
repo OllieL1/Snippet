b0306aa10c26857a9e78695c5fa918c763e0a286
OllieL1 <oliverlivingston14@gmail.com>
2024-10-16 22:41:57 +0100

    basic state structure in main.rs

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,3 +1,30 @@
+use std::io::{self,Write};
+
+enum State { // to be filled in
+    StateA,
+    StateB,
+    StateC,
+    StateD,
+    StateE,
+}
+
+impl State {
+    fn next(self) -> State {
+        match self { // to be filled in
+            State::StateA => State::StateB,
+            State::StateB => State::StateC,
+            State::StateC => State::StateD,
+            State::StateD => State::StateE,
+            State::StateE => State::StateA,
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut currentState = State::StateA; // starting state
+    // should be customisable
+
+    loop {
+        
+    }
 }
